f83bc603cd88db4f8aa0b4329a52d6dc603a89e8
Martina-commit <75865843+Martina-commit@users.noreply.github.com>
2020-12-20 02:31:31 -0300

    Add files via upload

---
+++ probandododod.cpp
@@ -0,0 +1,225 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+void mamita()
+{
+	int cont=0;
+	bool b=true;
+	
+	
+	
+	do
+	{
+		if (cont=5)
+		{	
+		  	system("cls");
+			system("color 0a");
+                                               printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+		    Sleep(500);
+		       if (cont=10)
+		       {
+		         system("cls");
+		         system("color 0b");	
+		 	     printf ("\t\t\t\t\t\n ********************************");
+                 printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                 printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                 printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                 printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                 printf ("\t\t\t\t\t\n ********************************");    
+                 printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                 printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                 printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                 printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                 printf ("\t\t\t\t\t\n *********************************");
+			     Sleep(500);
+				 
+				 		if (cont=10)
+		                {
+		                  system("cls");
+		                 system("color 0c");	
+			                                   printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+			            Sleep(500);	
+			            
+			            		if (cont=10)
+		                       {
+		                         system("cls");
+		                         system("color 0d");	
+			                                   printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+			                     Sleep(500);	
+			                     
+			                     	    if (cont = 15)
+	                                  {
+	                                    system("cls");
+		                                system("color 0c"); 	
+	    	                                   printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+	    	                             Sleep(500);	
+										 
+										 
+	                                    if (cont=20)
+	                                    {
+	                                      system("cls");
+		                                  system("color 0e"); 
+    	                                       printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+    	                                  Sleep(500);
+    	                                  
+    	                               if(cont=25)
+									   {
+									   	 system("cls");
+		                                 system("color 04"); 
+		                                       printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+									   	 Sleep(500);
+											
+										if(cont=30)
+										{
+											system("cls");
+		                                    system("color 06");
+		                                       printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+											Sleep(500);	
+											
+										if(cont=35)
+										{
+											system("cls");
+		                                    system("color 01"); 
+											   printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+											Sleep(500);
+										    if(cont=40)
+										    {
+											   system("cls");
+		                                       system("color 0f");
+										       printf ("\t\t\t\t\t\n ********************************");
+                                               printf ("\t\t\t\t\t\n ++++++ |¯ || ¯| +  ¯||¯¯¯| +++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||_|  ++++++");    
+                                               printf ("\t\t\t\t\t\n ++++++++  ||  ++++  ||-|  ++++++");    
+                                               printf ("\t\t\t\t\t\n +++++++ _ || _ ++  _||___| +++++");    
+                                               printf ("\t\t\t\t\t\n ********************************");    
+                                               printf ("\t\t\t\t\t\n + _|¯¯¯|_ + ||¯_¯|| +  _|¯¯¯|_ +");    
+                                               printf ("\t\t\t\t\t\n + ||+++|| + ||+¦+|| +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||¯¯¯|| + ||   || +  ||+++|| +");    
+                                               printf ("\t\t\t\t\t\n + ||   || + ||   || +  ¯|___|¯ +");    
+                                               printf ("\t\t\t\t\t\n *********************************");
+                                               Sleep(500);
+											       
+                                               if(cont=45)
+									           {
+										         b=false;
+									            }
+										    }	
+										}
+									}		   	
+								}    		  		
+		                    }			 	
+		                }
+	 	            }
+	 	        }	 	
+		    }  
+		}
+      
+          cont++;
+	
+
+
+	    }while (b==true);
+
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+main ()
+{
+	mamita();
+	
+	printf ("\nHola ma, bueno este es un pequeño regalito, gracias por haber estado este año acompañandome"
+    "\nse que tu apoyo fue fundamental para mi cursada, desde un consejo hasta tus puntos de vista," 
+	"\neste es un pequeño programa, pero se que en un futuro voy a poder armarte cosas que" 
+	"\nte sorprenda, en fin, gracias por estar.");
+}
+
+
